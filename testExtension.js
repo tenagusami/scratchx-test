4e9d08bc86901d51a5f51cd4912b369d6bfc11ac
tenagusami <tenagusami_mail@yahoo.co.jp>
2017-05-26 11:17:13 +0900

    partially Japanized

--- testExtension.js
+++ testExtension.js
@@ -1,39 +1,38 @@
-//(function (ext) {
 (() => {
-    let ext=this;
-    
-    // Cleanup function when the extension is unloaded
-    ext._shutdown = ()=>{};
+  let ext=this;
+  
+  // Cleanup function when the extension is unloaded
+  ext._shutdown = ()=>{};
 
-    // Status reporting code
-    // Use this to report missing hardware, plugin or unsupported browser
-    ext._getStatus = ()=>{
-        return {status: 2, msg: 'Ready'};
-    };
+  // Status reporting code
+  // Use this to report missing hardware, plugin or unsupported browser
+  ext._getStatus = ()=>{
+    return {status: 2, msg: 'Ready'};
+  };
 
-    ext.power = (base, exponent) => {
-        return Math.pow(base, exponent);
-    };
+  ext.べきじょう = (てい, しすう) => {
+    return Math.pow(てい, しすう);
+  };
 
-    ext.wait_random = (callback) => {
-        let wait = Math.random();
-        //console.log('Waiting for ' + wait + ' seconds');
-        window.setTimeout(function() {
-            callback();
-        }, wait*1000);
-    };
+  ext.ランダムにまつ = (コールバック) => {
+    let まつじかん = Math.random();
+    //console.log('Waiting for ' + wait + ' seconds');
+    window.setTimeout(()=>{
+      コールバック();
+    }, まつじかん*1000);
+  };
 
-    // Block and block menu descriptions
-    let descriptor = {
-        blocks: [
-            // Block type, block name, function name
-            ['r', '%n ^ %n', 'power', 2, 3],
-	    //['w', 'my first block', 'my_first_block'],
-	    //['w', 'my first block', 'wait_random'],
-	    ['w', 'wait for random time', 'wait_random'],
-        ]
-    };
+  // Block and block menu descriptions
+  let descriptor = {
+    blocks: [
+      // Block type, block name, function name
+      ['r', '%n ^ %n', 'べきじょう', 2, 3],
+      //['w', 'my first block', 'my_first_block'],
+      //['w', 'my first block', 'wait_random'],
+      ['w', 'ランダムにまつ', 'ランダムにまつ'],
+    ]
+  };
 
-    // Register the extension
-    ScratchExtensions.register('My first extension', descriptor, ext);
+  // Register the extension
+  ScratchExtensions.register('My first extension', descriptor, ext);
 })();
