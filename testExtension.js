f1d79fb6dd00fd7dd1b8b79ddaf08580fdb7f8f1
tenagusami <tenagusami_mail@yahoo.co.jp>
2017-05-26 12:20:44 +0900

    list and object test

--- testExtension.js
+++ testExtension.js
@@ -22,17 +22,38 @@
     }, まつじかん*1000);
   };
 
+  ext.リストをかえすテスト=(x1,x2)=>{
+    return [x1,x2,[x1,x2]];
+  };
+
+  ext.オブジェクトをかえすテスト=(x1,x2)=>{
+    return {test1:x1,test2:x2};
+  };
+  
+  ext.リストをよむテスト=(リスト)=>{
+    return リスト[2];
+  };
+
+  ext.オブジェクトをよむテスト=(オブジェクト)=>{
+    return オブジェクト.test1;
+  };
+
   // Block and block menu descriptions
   let descriptor = {
     blocks: [
       // Block type, block name, function name
       ['r', '%n ^ %n', 'べきじょう', 2, 3],
+      ['r', '%n , %n', 'リストをかえすテスト', 2, 3],
+      ['r', '%s をよむ', 'リストをよむテスト', 'a'],
+      ['r', '{ %n , %n }', 'オブジェクトをかえすテスト', 2,3],
+      ['r', '%s .test1', 'オブジェクトをよむテスト', ''],
       //['w', 'my first block', 'my_first_block'],
       //['w', 'my first block', 'wait_random'],
-      ['w', 'ランダムにまつ', 'ランダムにまつ'],
+      //['w', 'ランダムにまつ', 'ランダムにまつ'],
     ]
   };
-
+  
   // Register the extension
   ScratchExtensions.register('My first extension', descriptor, ext);
 })();
+
